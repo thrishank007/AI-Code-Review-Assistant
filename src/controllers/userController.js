acf9d9a11d0168a9cae6f7357195339e822ef1a1
Thrishank <thrishankchintham@gmail.com>
2025-01-09 08:43:23 +0530

    ft. Fixed Username duplication

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,13 +7,17 @@ const registerUser = async (req, res) => {
 
   try {
     // Check if user already exists
+    const existingUsername = await User.findOne({ where: { username } });
+    if (existingUsername) {
+      return res.status(400).json({ message: 'Username already exists' });
+    }
     const existingUser = await User.findOne({ where: { email } });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
     // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 4);
 
     // Create new user
     const newUser = await User.create({
